f6722d9d805ba12f00b394aad4002a6888c76c99
Mattia Cugusi <m.cugusi287gmail.com>
2024-10-12 19:20:22 +0200

    inizio progetto

---
+++ VideoYouTube.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Video YouTube</title>
+</head>
+<body>
+    <?php
+     $video1 = array("titolo" => "Fiorentina - Juventus 4-2 - Sky Sport Higlights - Serie A", "idVideo" => "NekltLjByrE");
+     $video2:
+     $video3;
+
+    ?>
+</body>
+</html>
